6b41190070c3298ab7a9e84bf3a1bd5a210dcce0
Mai Pham <maitphm@gmail.com>
2020-12-14 22:49:55 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,12 +3,12 @@ class Song < ActiveRecord::Base
   belongs_to :genre 
   has_many :notes
 
-  def genre_name=(name)
-    self.genre = Genre.find_or_create_by(name: name)
-  end
+  # def genre_name=(name)
+  #   self.genre = Genre.find_or_create_by(name: name)
+  # end
 
-  def genre_name
-    self.genre.name if self.genre
+  # def genre_name
+  #   self.genre.name if self.genre
   end
 
   def artist_name=(name)
